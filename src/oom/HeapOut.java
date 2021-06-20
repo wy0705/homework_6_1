f1f62b72ba43a39eb1d780df529f8f08b510e004
wy0705 <2652918156@qq.com>
2021-06-20 23:12:53 +0800

    22

--- src/oom/HeapOut.java
+++ src/oom/HeapOut.java
@@ -1,11 +1,13 @@
 package oom;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class HeapOut {
     public static void main(String[] args) throws InterruptedException {
         int size=1024*1024*8;
+        HashMap hashMap;
         List<byte[]> list=new ArrayList<byte[]>();
         while(true) {
             Thread.sleep(1);
